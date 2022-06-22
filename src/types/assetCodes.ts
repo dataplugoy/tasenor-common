c40bc1a3c221d3f0177e55fd6b5d61a9a10e0a63
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2022-06-22 13:05:52 +0300

    More codes.

--- src/types/assetCodes.ts
+++ src/types/assetCodes.ts
@@ -1,26 +1,72 @@
 /**
  * An asset type
  *
+ * * `ACCRUED_INCOME` - Prepayments and accrued income
+ * * `APPROPRIATIONS` - Appropriations
  * * `ASSETS` - Assets
+ * * `BONDS` - Bonds
+ * * `CAPITAL_RESERVERS` - Capital and reserves
  * * `CAPITAL_RESERVERS_AND_LIABILITIES` - Capital, reserves and liabilities
- * * `CASH_AT_BANK_AND_IN_HAND` - Cash at bank and in hand
+ * * `CASH` - Cash at bank and in hand
+ * * `CONVERTIBLE_BONDS` - Convertible bonds
+ * * `CREDITORS` - Creditors
+ * * `CRYPTOCURRENCIES` - Cryptocurrencies
  * * `CURRENT_ASSETS` - Current assets
  * * `CURRENT_DEBTORS` - Debtors
  * * `CURRENT_INVESTMENTS` - Investments
+ * * `CURRENT_PRIVATE_STOCK_SHARES` - Unlisted shares
+ * * `CURRENT_PUBLIC_STOCK_SHARES` - Publicly listed shares
  * * `CURRENT_STOCKS` - Stocks
+ * * `GROUP_UNDERTAKINGS_HOLDINGS` - Holdings in group undertakings
  * * `INTANGLIBLE_ASSETS` - Intanglible assets
  * * `INVESTMENTS` - Investments
+ * * `LIABILITITES_TO_CREDIT_INSTITUTIONS` - Liabilities to credit institutions
+ * * `LOANS_RECEIVABLE` - Loans receivable
  * * `NON_CURRENT_ASSETS` - Non-current assets
+ * * `OTHER_CREDITORS` - Other creditors
+ * * `OTHER_DEBTORS` - Other debtors
+ * * `OTHER_INVESTMENTS` - Other investments
+ * * `OTHER_SHARES` - Other shares and similar ownerships
+ * * `OWNED_BY_GROUP` - Amounts owned by group undertakings
+ * * `OWNED_BY_PARTICIPATING` - Amounts owned by participating interest undertakings
+ * * `PROVISIONS` - Provisions
+ * * `SHORT_SELLING_STOCKS` - Short-selling stocks
+ * * `SUBSCRIBED_CAPITAL` - Subscribed capital
+ * * `SUBSRIBED_CAPITAL_UNPAID` - Subscribed capital unpaid
  * * `TANGLIBLE_ASSETS` - Intanglible assets
+ * * `TRADE_DEBTORS` - Trade debtors
  */
-export type AssetCode = 'ASSETS' |
+export type AssetCode = 'ACCRUED_INCOME' |
+  'APPROPRIATIONS' |
+  'ASSETS' |
+  'BONDS' |
+  'CAPITAL_RESERVERS' |
   'CAPITAL_RESERVERS_AND_LIABILITIES' |
-  'CASH_AT_BANK_AND_IN_HAND' |
+  'CASH' |
+  'CONVERTIBLE_BONDS' |
+  'CREDITORS' |
+  'CRYPTOCURRENCIES' |
   'CURRENT_ASSETS' |
   'CURRENT_DEBTORS' |
   'CURRENT_INVESTMENTS' |
+  'CURRENT_PRIVATE_STOCK_SHARES' |
+  'CURRENT_PUBLIC_STOCK_SHARES' |
   'CURRENT_STOCKS' |
+  'GROUP_UNDERTAKINGS_HOLDINGS' |
   'INTANGLIBLE_ASSETS' |
   'INVESTMENTS' |
+  'LIABILITITES_TO_CREDIT_INSTITUTIONS' |
+  'LOANS_RECEIVABLE' |
   'NON_CURRENT_ASSETS' |
-  'TANGLIBLE_ASSETS'
+  'OTHER_CREDITORS' |
+  'OTHER_DEBTORS' |
+  'OTHER_INVESTMENTS' |
+  'OTHER_SHARES' |
+  'OWNED_BY_GROUP' |
+  'OWNED_BY_PARTICIPATING' |
+  'PROVISIONS' |
+  'SHORT_SELLING_STOCKS' |
+  'SUBSCRIBED_CAPITAL' |
+  'SUBSRIBED_CAPITAL_UNPAID' |
+  'TANGLIBLE_ASSETS' |
+  'TRADE_DEBTORS'
