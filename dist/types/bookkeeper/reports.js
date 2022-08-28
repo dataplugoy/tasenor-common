15b4f7cb7ab9db9600ad3a22b0d89691292962ea
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2022-08-28 17:19:57 +0300

    Switch to ebuild.

--- dist/types/bookkeeper/reports.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isReportID = void 0;
-function isReportID(s) {
-    return typeof s === 'string' && /^[-a-z]+$/.test(s);
-}
-exports.isReportID = isReportID;
-//# sourceMappingURL=reports.js.map
--- dist/types/bookkeeper/risp_types.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=risp_types.js.map
