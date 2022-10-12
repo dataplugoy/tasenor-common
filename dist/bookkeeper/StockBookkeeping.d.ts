3726f618272b7046d3c6edfcc5eb3a64f4fa1163
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2022-10-12 20:34:49 +0300

    Rebuild.

--- dist/bookkeeper/StockBookkeeping.d.ts
+++ dist/bookkeeper/StockBookkeeping.d.ts
@@ -106,6 +106,12 @@ export declare class StockBookkeeping {
      * @returns
      */
     total(type: AssetType | Asset, asset?: Asset | undefined): number;
+    /**
+     * Get the latest value of one asset.
+     * @param type
+     * @param asset
+     * @returns
+     */
     value(type: AssetType | Asset, asset?: Asset | undefined): number;
     /**
      * Collect full stock summary.
